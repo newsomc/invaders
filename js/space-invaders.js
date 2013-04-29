3c4e2954c221ae90c0eacca8bf745c7f52d12a8f
Clint Newsom <hcnewsom@gmail.com>
2013-04-29 15:58:10 -0400

    Bullets and Aliens now follow prototypal inheritance model.

--- js/space-invaders.js
+++ js/space-invaders.js
@@ -8,26 +8,30 @@
 
    var stats = new Stats();
    stats.setMode(0); // 0: fps, 1: ms
-   
-   // Align stats top-left
    stats.domElement.style.position = 'absolute';
    stats.domElement.style.left = '1215px';
    stats.domElement.style.top = '0px';
    document.body.appendChild( stats.domElement );
 
+   //Globals...encapsulate in an APP class.
    var canvasWidth = 780, canvasHeight = 620,
    c = document.getElementById("space-invaders"),
    ctx = c.getContext("2d"), 
    fps = 60, interval = 1000 / fps, 
    bullets = [], aliens = [], 
-   particles = [];
+   particles = [],
+   phase = "waiting";
 
 	 var keyHandlers = {
 		 32: 'shoot',
 		 37: 'moveLeft',
-		 39: 'moveRight'
+		 39: 'moveRight',
+     83: 'start'
 	 };
 
+   /**
+    * User
+    */
    var User = function(){
      this.color = "#00A";
      this.x = 25;
@@ -39,25 +43,27 @@
      this.maxSpeed = 10;
      this.friction = 0.93;
      this.score = 0;
+     this.active = true;
      this.lives = 3;
    };
 
    User.prototype.shoot = function(){
      this.midpoint = {
-         x : this.x + this.width / 2,
-         y : this.y + this.height / 2
-       };
+       x : this.x + this.width / 2,
+       y : this.y + this.height / 2
+     };
 
      bullets.push(
-       Amo({
-              speed : 5,
-              x : this.midpoint.x,
-              y : this.midpoint.y
-       }));
+       new Bullet({
+         speed : 5,
+         x : this.midpoint.x,
+         y : this.midpoint.y
+     }));
    };
 
-   User.prototype.move = function (dir) {
+   User.prototype.move = function(dir) {
      this.dir = dir;
+
      if (this.dir === "moveLeft") {
        if (this.velocityX > -this.speed) {
          this.velocityX--;
@@ -68,61 +74,88 @@
      }
    };
 
-   User.prototype.event = function(a, e){
+   User.prototype.accelerate = function(friction, velocity) {
+     this.friction  = friction;
+     this.velocityX = velocity;
+     this.velocityX *= this.friction;
+     this.x += this.velocityX;
+   };
+
+   //Needs to be pulled out. Should be moved to an App handler.
+   User.prototype.event = function(a, e) {
      if (a == "shoot") {
        this.shoot();
+     }
+     else if (a == "start"){
+       phase = "playing";
      }
      else {
        this.move(a);
      }
    };
    
-   User.prototype.draw = function(){
+   User.prototype.draw = function() {
      ctx.fillStyle = this.color;
      ctx.fillRect(this.x, this.y, this.width, this.height);
    };
 
-   User.prototype.explode = function(){
-     //some code.
+   User.prototype.explode = function(x, y) {
+     var particleAmt = Math.random()*5 + 5;
+     for (var i = 0; i < particleAmt; i++) {
+       var dir   = Math.random()*2*Math.PI;
+       var speed = Math.random()*3 + 2;
+       var life  = Math.random()*10 + 10;
+       var color = "#00A";
+       particles[particles.length] = new Particle(x, y, speed, dir, life, color);
+     }
    };
 
-   User.prototype.incrementScore = function(){
+   User.prototype.incrementScore = function() {
      this.score ++;
    };
 
-   User.prototype.decrementLives = function(){
+   User.prototype.decrementLives = function() {
      this.lives --;
    };
 
-   var Amo = function (bullet) {
-     bullet.active = true;
-     bullet.xVelocity = 0;
-     bullet.yVelocity = -bullet.speed;
-     bullet.width = 3;
-     bullet.height = 3;
-     bullet.color = "#3366FF";
-     
-     bullet.inBounds = function() {
-       return bullet.x >= 0 && bullet.x <= canvasWidth && 
-         bullet.y >= 0 && bullet.y <= canvasHeight;
-     };
-     
-     bullet.draw = function() {
-       ctx.fillStyle = this.color;
-       ctx.fillRect(this.x, this.y, this.width, this.height);
-     };
+   /**
+    * Bullets.
+    */
 
-     bullet.update = function() {
-       bullet.x += bullet.xVelocity;
-       bullet.y += bullet.yVelocity;
-       bullet.active = bullet.active && bullet.inBounds();
-     };
-
-     return bullet;
+   var Bullet = function (bullet) {
+     this.x = bullet.x;
+     this.y = bullet.y;
+     this.speed = bullet.speed;
+     this.active = true;
+     this.xVelocity = 0;
+     this.yVelocity = -bullet.speed;
+     this.width = 3;
+     this.height = 3;
+     this.color = "#3366FF";
    };
 
-   var Aliens = function (alien) {
-     alien = alien || {};
+   Bullet.prototype.inBounds = function() {
+     return this.x >= 0 && this.x <= canvasWidth && 
+       this.y >= 0 && this.y <= canvasHeight;
+   };
+     
+   Bullet.prototype.draw = function() {
+     ctx.fillStyle = this.color;
+     ctx.fillRect(this.x, this.y, this.width, this.height);
+   };
+
+   Bullet.prototype.update = function() {
+     this.x += this.xVelocity;
+     this.y += this.yVelocity;
+     this.active = this.active && this.inBounds();
+     //console.log(this.x, this.y, this.active);
+   };
+
+   /**
+    * Aliens
+    */
+   var Alien = function (alien) {
+     alien = this;
      alien.alive = true;
      alien.age = Math.floor(Math.random() * 120);
      alien.color = "#00FF00";
@@ -132,63 +165,60 @@
      alien.velocityY = 2;
      alien.width = 7;
      alien.height = 3;
-
-     alien.inBounds = function () {
-       return alien.x >= 0 && alien.x <= canvasWidth && 
-         alien.y >= 0 && alien.y <= canvasHeight;
-     };
-     
-     alien.draw = function() {
-       if(alien.alive){
-         ctx.fillStyle = this.color;
-         ctx.fillRect(this.x, this.y, this.width, this.height);
-       }
-     };
-     
-     alien.update = function() {
-       alien.x += alien.velocityX;
-       alien.y += alien.velocityY;
-       alien.velocityX = 3 * Math.sin(alien.age * Math.PI / 64);
-       alien.age++;
-       alien.alive = alien.alive && alien.inBounds();
-     };
-     
-     alien.explode = function(x, y) {
-       var particleAmt = Math.random()*20 + 50;
-       alien.alive = false;
-       for (var i = 0; i < particleAmt; i++) {
-         var dir = Math.random()*2*Math.PI;
-         var speed = Math.random()*3 + 2;
-         var life = Math.random()*10 + 10;
-         particles[particles.length] = new Particle(x, y, speed, dir, life);
-       }
-     };
-
-     return alien;
    };
 
-   //Explosions.
-   var Particle = function(x, y, speed, dir, life) {
-
+   Alien.prototype.inBounds = function () {
+     return this.x >= 0 && this.x <= canvasWidth && 
+       this.y >= 0 && this.y <= canvasHeight;
+   };
+   
+   Alien.prototype.draw = function() {
+     if (this.alive) {
+       ctx.fillStyle = this.color;
+       ctx.fillRect(this.x, this.y, this.width, this.height);
+     }
+   };
+   
+   Alien.prototype.update = function() {
+     this.x += this.velocityX;
+     this.y += this.velocityY;
+     this.velocityX = 3 * Math.sin(this.age * Math.PI / 64);
+     this.age++;
+     this.alive = this.alive && this.inBounds();
+   };
+   
+   Alien.prototype.explode = function(x, y) {
+     var particleAmt = Math.random()*20 + 50;
+     this.alive = false;
+     for (var i = 0; i < particleAmt; i++) {
+       var dir = Math.random()*2*Math.PI;
+       var speed = Math.random()*3 + 2;
+       var life = Math.random()*10 + 10;
+       particles[particles.length] = new Particle(x, y, speed, dir, life, alien.color);
+     }
+   };
+   
+   //Dealing with explosions.
+   var Particle = function(x, y, speed, dir, life, color) {
      this.x = x;
      this.y = y;
      this.life = life;     
-     
-     var incX = Math.cos(dir) * speed;
-     var incY = Math.sin(dir) * speed;
-    
-     this.update = function() {
-       this.x += incX;
-       this.y += incY;
-       this.life--;        
-    };
-
+     this.color = color;
+     this._incX = Math.cos(dir) * speed;
+     this._incY = Math.sin(dir) * speed;
    };
-     
+   
+   Particle.prototype.update = function() {
+     this.x += this._incX;
+     this.y += this._incY;
+     this.life--;
+   };
+
+   // Start global particle fncs.
    var drawParticles = function() {
-     ctx.fillStyle = "#00FF00";
      removeParticles();
      particles.forEach(function(p){
+       ctx.fillStyle = p.color;
        ctx.fillRect(p.x-1, p.y-1, 1, 1); 
        p.update();
      });
@@ -214,14 +244,19 @@
      t = setTimeout(animateParticles, 33);
    };
 
-   //Collisions.
-   function collision(a, b) {
+   // End Particle fncs.
+
+
+   // Collisions.
+   var collision = function(a, b) {
      return a.x < b.x + b.width &&
        a.x + a.width > b.x &&
        a.y < b.y + b.height &&
        a.y + a.height > b.y;
-   }
+   };
 
+
+   // This is where App should probably start. 
    var handleCollisions = function() {
      bullets.forEach(function(bullet) {
        aliens.forEach(function(alien) {
@@ -232,19 +267,18 @@
          }
        });
      });
-
      aliens.forEach(function(alien){
        if(collision(alien, user)){
-         alien.explode();
-         user.explode();
+         //alien.explode();
+         user.explode(user.x, user.y);
          user.decrementLives();
        }
      });
    };
 
-   function update(){
-
+   var update = function() {
      bullets.forEach(function(bullet){
+       //console.log(bullet);
        bullet.update();
      });
 
@@ -253,8 +287,7 @@
      });     
 
      //User friction/velocity.
-     user.velocityX *= user.friction;
-     user.x += user.velocityX;
+     user.accelerate(user.friction, user.velocityX);
 
      //Aliens
      aliens.forEach(function(alien) {
@@ -266,40 +299,39 @@
      });
      
      if(Math.random() < 0.1) {
-       aliens.push(Aliens());
+       aliens.push(new Alien);
      }
 
      handleCollisions();
-   }
+   };
    
-   function animate(){
+   var animate = function() {
      ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-
      user.draw();
-
      bullets.forEach(function(bullet){
        bullet.draw();               
      });
-
      aliens.forEach(function(alien) {
        alien.draw();
      });
-   }
+   };
 
-   function draw() {
+   var mainDraw = function() {
+
      setTimeout(function() {
        stats.begin();
-       window.requestAnimationFrame(draw);
+       window.requestAnimationFrame(mainDraw);
        update();
        animate();
        updateScore();
        stats.end();
      }, interval);
+
    };
 
    //Score panel
-   var updateScore = function(){
-     ctx.lineWidth=0.5;
+   var updateScore = function() {
+     ctx.lineWidth = 0.5;
      ctx.font="9px sans-serif";
      ctx.textAlign = "right";
      ctx.textBaseline = "bottom";
@@ -307,20 +339,25 @@
      ctx.fillText("Lives: " + user.lives, 300, 135);     
    };
 
+   var updateStatus = function(msg) {
+     ctx.lineWidth = 0.5;
+     ctx.font="9px sans-serif";
+     ctx.fillText(msg, 50, 135);     
+   };
+
    // Key handlers
-	 function handleKeys(){
+	 var handleKeys = function() {
 		 document.addEventListener('keydown', function(e){
 		   if( keyHandlers[e.keyCode] ) {
          user.event(keyHandlers[e.keyCode], e);         
        }
      });
-	 }
+	 };
 
    var user = new User;
-   var alien = new Aliens;
-
+   var alien = new Alien;
    handleKeys();
-   draw();
+   mainDraw();
    animateParticles();
 
-}());
+ }());
